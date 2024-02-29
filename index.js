7ed9990e898d0134ff99b8e360b297a7a3c49111
ThakareSakshi <sakshithakare121@gmail.com>
2024-02-29 23:38:08 +0530

    uploaede

---
+++ index.js
@@ -0,0 +1,63 @@
+import express from "express"; 
+import fs from "node:fs";
+import path from "node:path";
+import { nanoid } from "nanoid";
+import { fileURLToPath } from "node:url";
+
+const app=express();
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const isURLValid = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log("Request received on /",__dirname);
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/url-shortner", (req, res) => {
+  const longUrl = req.body.longUrl;
+  const shortUrl = nanoid(10);
+  console.log(req.body.url);
+  
+ 
+  const fileResponse = fs.readFileSync(path.join(__dirname,"urlmap.json"));
+  console.log(fileResponse);
+  const fileData = JSON.parse(fileResponse.toString());
+  console.log(fileData)
+
+  fileData[shortUrl] = longUrl;
+
+  fs.writeFileSync(path.join(__dirname,"urlmap.json"), JSON.stringify(fileData));
+  console.log(fileData)
+
+  res.json({
+    succes: true,
+    url: `http://localhost:5000/${shortUrl}`,
+  });
+});
+
+app.get("/:shortUrl", (req, res) => {
+  const fileResponse = fs.readFileSync(path.join(__dirname,"urlmap.json"));
+  const fileData = JSON.parse(fileResponse.toString());
+  const longUrl = fileData[req.params.shortUrl];
+  console.log(longUrl);
+  // If longurl doesn't exists show 404 with json
+  res.redirect(longUrl);
+});
+
+app.listen(5000,()=>{
+    console.log("app running");
+})
